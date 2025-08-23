e8a105c09a7aedcf79b63595f8db8aa06222ee5e
schweitzer <schweitzeraa16@gmail.com>
2025-08-23 15:47:25 +0200

    separated client modifications into client2 for faster and cleaner updates from upstream

--- cmd/examples/media.h
+++ cmd/examples/media.h
@@ -40,7 +40,6 @@ enum class TrackType
 
 class Track
 {
-  std::deque<MP4Chunk> chunks;
   std::mutex t_mtx;
   std::condition_variable cv;
 
@@ -52,36 +51,6 @@ public:
 
   Track(int idx, const std::string track_name)
       : index(idx), name(track_name) {}
-
-  void PutChunk(const MP4Chunk chunk)
-  {
-    MP4Chunk chunk_copy = chunk;
-    {
-      std::lock_guard<std::mutex> lock(t_mtx);
-      chunks.push_back(chunk_copy);
-    }
-    cv.notify_one();
-  }
-
-  MP4Chunk GetChunk(const bool& exit)
-  {
-    std::unique_lock<std::mutex> lock(t_mtx);
-    if (!chunks.empty()) {
-      MP4Chunk chunk = chunks.front();
-      chunks.pop_front();
-      return chunk;
-    }
-    cv.wait(lock, [this, exit]() { return !chunks.empty() || exit;});
-    if (exit) {return MP4Chunk();} // Return empty chunk if exit is requested
-    MP4Chunk chunk = chunks.front();
-    chunks.pop_front();
-    return chunk;
-  }
-
-  void NotifyAll()
-  {
-    cv.notify_all();
-  }
 
   virtual ~Track() = default;
 };
@@ -100,11 +69,8 @@ class SoundTrack : public Track
     SoundTrack(int idx, const std::string track_name) : Track(idx,track_name) { type = TrackType::AUDIO; }
 };
 
-struct SharedState {
-  std::vector<std::shared_ptr<Track>> tracks;
-  std::deque<MP4Chunk> chunks;
-  std::vector<quicr::FullTrackName> track_names;
-  bool catalog_ready = false;
+class SharedState {
+  std::deque<std::shared_ptr<MP4Chunk>> chunks;
 
   MP4Atom ftyp;
   MP4Atom moov;
@@ -112,11 +78,24 @@ struct SharedState {
   std::mutex s_mtx;
   std::condition_variable cv;
 
+public:
+    bool catalog_ready = false;
+    std::vector<std::shared_ptr<Track>> tracks;
+    std::vector<quicr::FullTrackName> track_names;
+
   void AddTrack(std::shared_ptr<Track> track)
   {
     std::lock_guard<std::mutex> lock(s_mtx);
-        tracks.push_back(track);
-        std::cout << "Track added to shared state, Tracks: " << tracks.size() << std::endl;
+    tracks.push_back(track);
+    std::cout << "Track added to shared state, Tracks: " << tracks.size() << std::endl;
+  }
+
+  void WaitForCatalogReady(const bool& stop_threads)
+  {
+    std::unique_lock<std::mutex> lock(s_mtx);
+      cv.wait(lock, [&]() {
+          return catalog_ready || stop_threads;
+      });
   }
 
   MP4Atom getFtyp() {
@@ -141,31 +120,32 @@ struct SharedState {
     cv.notify_all();
   }
 
-  void PutChunk(const MP4Chunk& chunk)
+  void PutChunk(std::shared_ptr<MP4Chunk> chunk)
   {
     {
       std::lock_guard<std::mutex> lock(s_mtx);
       chunks.push_back(chunk);
-      std::cout << "Chunk added to shared state, Chunks: " << chunks.size() << std::endl;
+      //std::cout << "Chunk added to shared state, Chunks: " << chunks.size() << std::endl;
     }
     cv.notify_one();
   }
 
-  MP4Chunk GetChunk(const std::atomic<bool>& exit)
+  std::shared_ptr<MP4Chunk> GetChunk(const bool& exit)
   {
-    std::unique_lock<std::mutex> lock(s_mtx);
-    if (!chunks.empty()) {
-      MP4Chunk chunk = chunks.front();
+      std::unique_lock<std::mutex> lock(s_mtx);
+      while (chunks.empty() && !exit) {
+          //std::cout << "-----Waiting for chunks in shared state..." << std::endl;
+          cv.wait_for(lock, std::chrono::milliseconds(1000));
+          //std::cout  << "wait returned: " << chunks.size() << std::endl;
+      }
+      if (exit || chunks.empty()) {
+          return std::make_shared<MP4Chunk>(); // Return empty chunk if exit or still no chunk
+      }
+      std::shared_ptr<MP4Chunk> chunk = chunks.front();
+      //std::cout << "shared pointer body count before pop: " << chunk.use_count() << std::endl;
       chunks.pop_front();
-      std::cout << "Chunks in shared state: " << chunks.size() << std::endl;
+      //std::cout << "Chunk deleted from shared state, Chunks: " << chunks.size() << std::endl;
       return chunk;
-    }
-    cv.wait(lock, [this, &exit]() { return !chunks.empty() || exit;});
-    if (exit) {return MP4Chunk();} // Return empty chunk if exit is requested
-    MP4Chunk chunk = chunks.front();
-    chunks.pop_front();
-    std::cout << "Chunk deleted from shared state, Chunks: " << chunks.size() << std::endl;
-    return chunk;
   }
 
   void NotifyAll()
@@ -174,15 +154,17 @@ struct SharedState {
   }
 };
 
-void DoParse(const std::shared_ptr<SharedState>& shared_state, const std::atomic<bool>& stop);
+void DoParse(const std::shared_ptr<SharedState>& shared_state, const bool& stop);
+void DoParse2(const std::shared_ptr<SharedState>& shared_state, const bool& stop);
 
 class CMafParser
 {
   SharedState& state;
 public:
-  CMafParser(SharedState& shared_state) : state(shared_state) {}
+  CMafParser(SharedState& shared_state) : state(shared_state) {
+  }
 
-  int ExtractTrackIdFromMoof(const MP4Atom& moof);
+  static int ExtractTrackIdFromMoof(const MP4Atom& moof);
   static bool HasKeyframe(const MP4Atom& moof);
   void ParseBuffer(const uint8_t* buffer, size_t buffer_size);
 
